ea23b683591e7d56028ceb8b66dfbfc59b946059
pengzhangg <pengzhang720@gmail.com>
2025-12-27 10:57:38 +1300

    implemented sign in/sign out functionality to navbar

---
+++ video_web_client/app/util/firebase.ts
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, 
+    signInWithPopup, 
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    User
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjtWN6z07Zc2QKyECXZQ0B81lhRvNv0mk",
+  authDomain: "video-streaming-site-bf57e.firebaseapp.com",
+  projectId: "video-streaming-site-bf57e",
+  appId: "1:425586204692:web:14879120cc1f6784f1c701"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+export function signOut() {
+    return auth.signOut();
+}
+
+// trigger callback when auth state changes
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+}
